4703430453cc982a93f7f8ce50cee1b996b413ec
Alasdair McLeay <alasdair.mcleay@me.com>
2019-11-15 08:43:59 +0000

    curl 1.1.0

--- optional-curl-npm.sh
+++ optional-curl-npm.sh
@@ -1,8 +1,11 @@
 # these tgz are already stored in the git repo but can be redownloaded if needed
 mkdir from-npm && cd from-npm
 mkdir no-namespace && cd no-namespace
+mkdir 1.0.0 && cd 1.0.0
 curl https://registry.npmjs.org/eslint-visitor-keys/-/eslint-visitor-keys-1.0.0.tgz -o eslint-visitor-keys-1.0.0.tgz
-cd ..
+cd .. && mkdir 1.1.0 && cd 1.1.0
+curl https://registry.npmjs.org/eslint-visitor-keys/-/eslint-visitor-keys-1.1.0.tgz -o eslint-visitor-keys-1.1.0.tgz
+cd ../..
 mkdir types-namespace && cd types-namespace
 curl https://registry.npmjs.org/@types/eslint-visitor-keys/-/eslint-visitor-keys-1.0.0.tgz -o eslint-visitor-keys-1.0.0.tgz
 
